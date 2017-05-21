595c6110ed5983462db7a6b76f989d0d52557a14
Vincent Chapron <vincent@chapron.io>
2017-05-21 15:06:53 +0200

    Update tests

--- src/tests/app/kernel-test.ts
+++ src/tests/app/kernel-test.ts
@@ -2,35 +2,29 @@ import * as fs from 'fs';
 import * as path from 'path';
 import * as mkdirp from 'mkdirp';
 import {expect} from 'chai';
-import {ModuleProvider} from '../../';
+import {ModuleProvider, ViewProvider} from '../../';
 import AppKernel from './AppKernel';
 
 describe('App > Kernel >', function() {
     it('should load AppModule in the kernel', function() {
-        let kernel = new AppKernel();
-        let _kernel = (kernel as any);
-        let modules = _kernel.moduleProvider.getModules().filter(m => (m.name() === 'AppModule'));
+        let modules = ModuleProvider.getInstance().getModules().filter(m => (m.name() === 'AppModule'));
 
         expect(modules).to.have.lengthOf(1);
     });
 
     it('sould add AppBundle views directory to the kernel', function() {
-        let kernel = new AppKernel();
-        let _kernel = (kernel as any);
-        let modules = _kernel.moduleProvider.getModules().filter(m => (m.name() === 'AppModule'));
+        let modules = ModuleProvider.getInstance().getModules().filter(m => (m.name() === 'AppModule'));
 
         let viewsDir = modules[0].getViewsDir();
         if (!(fs.existsSync(viewsDir) && fs.lstatSync(viewsDir).isDirectory())) mkdirp.sync(viewsDir);
-        let viewsDirectories = kernel.generateViewsDirectories();
-        
+        let viewsDirectories = ViewProvider.getInstance().getViewDirectories();
+
         expect(viewsDirectories).to.have.lengthOf(1);
         expect(viewsDirectories[0]).to.be.equal(viewsDir);
     });
 
     it('sould generate good path from shortcut string', function() {
-        let kernel = new AppKernel();
-        let _kernel = (kernel as any);
-        let modules = _kernel.moduleProvider.getModules().filter(m => (m.name() === 'AppModule'));
+        let modules = ModuleProvider.getInstance().getModules().filter(m => (m.name() === 'AppModule'));
 
         let pathProvider = ModuleProvider.getInstance();
         let c1 = pathProvider.getDirname(modules[0], 'Posts/PostsController', 'Controller');
